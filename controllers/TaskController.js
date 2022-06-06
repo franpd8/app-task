c30596ca1c0929ee729bc205b77bc408c30987d9
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-06-06 10:52:02 +0200

    crud added

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -24,6 +24,46 @@ const TaskController = {
         console.error(error)
         res.status(500).send(error)
     }
-  }
+  },
+  async getById (req,res){
+    try{
+        const task = await Task.findById(req.params._id)
+        res.send({msg:'Tu vaina loca por id', task})
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+  },
+  async markAsCompleted (req,res){
+
+    try{
+        const taskCompleted = await Task.findOneAndUpdate({_id:req.params._id},{completed:true},{new:true})
+
+  } catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+},
+async update (req,res){
+    try{
+        const taskUpdated = await Task.findByIdAndUpdate(req.params._id,{title:req.body.title},{new:true})
+        res.send({msg:"tu vaina loca ha cambiado",taskUpdated})
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+},
+async delete (req,res){
+    try{
+        const taskDeleted = await Task.findByIdAndDelete(req.params._id)
+        res.send({msg:"tu vaina loca ha desaparecido",taskDeleted})
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
 };
 module.exports = TaskController
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 router.post('/add', TaskController.create)
 router.get('/all', TaskController.getAll)
-
+router.get('/search/:_id', TaskController.getById)
+router.put('/markAsCompleted/:_id', TaskController.markAsCompleted)
+router.put('/update/:_id', TaskController.update)
+router.delete('/delete/:_id', TaskController.delete)
 module.exports = router
